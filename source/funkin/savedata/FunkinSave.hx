b54956925b9a8ae982fbfdc60dacad7eea7fab39
SangMzG <levansang16102009ii@gmail.com>
2025-05-11 17:49:49 +0700

    fix modding once and for all

--- source/funkin/savedata/FunkinSave.hx
+++ source/funkin/savedata/FunkinSave.hx
@@ -27,7 +27,7 @@ class FunkinSave {
 		//trace(Application.current.meta.get('save-path'));
 		//trace(Application.current.meta.get('save-name'));
 		save = new FlxSave();
-		save.bind('save-default', #if sys 'YoshiCrafter29/CodenameEngine' #else 'CodenameEngine' #end);
+		save.bind('save-default', #if sys 'CodenameEngine/DeadlyDuet' #else 'CodenameEngine/DeadlyDuet' #end);
 		load();
 
 		if (!__eventAdded) {
